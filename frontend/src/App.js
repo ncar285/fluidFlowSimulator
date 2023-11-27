2e917f2c4fdcfe25eb85777a9189247a151e4a83
ncar285 <carlier.nicholas@gmail.com>
2023-11-28 12:19:08 +1300

    add seperated forntend and backend and styles setup on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
@@ -8,7 +7,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* Additional routes can be added here */}
         </Routes>
       </Router>
     );
