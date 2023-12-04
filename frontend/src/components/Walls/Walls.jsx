990f9c99842d265beeaef30401163397d9160223
ncar285 <carlier.nicholas@gmail.com>
2023-12-04 14:58:27 +1300

    draw generic shapes with vertices

--- frontend/src/components/Walls/Walls.jsx
+++ frontend/src/components/Walls/Walls.jsx
@@ -6,66 +6,44 @@ const Walls = ( shapeSettings ) => {
 
     const { type, parameters } = shapeSettings;
 
-    const createAxisArray = (size) => {
-        const axisArray = [(-1 / 2) * size];
-        const spacing = (size)/(resolution - 1)
-        for (let i = 1 ; i < resolution ; i++){
-            nextPos = axisArray[axisArray.length - 1] + spacing;
-            axisArray.push(nextPos);
-        }
-        return axisArray
-    }
-
-    const createCircle = () => {
-        const diametor = size;
-        const [h, k] = centre;
-        const xAxis = createAxisArray(diametor)
-        return xAxis.map((xPos)=>{
-            const yPos = k + Math.pow(2,r) - Math.pow(2, (x - h));
-            return [xPos + h, yPos + k]
-        })
-    }
-
     const createAerofoilVertices = () => {
         // finish this later
-        return [-10,10,-10,10];
+        return [[-10,-10],[-10,10],[10, 10],[10,-10]];
     }
-
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
         context.clearRect(0, 0, canvas.width, canvas.height);
-        context.beginPath();
 
         switch (type) {
             case 'circle':
-                const [h, k] = parameters.centre;
-                const r = parameters.radius;
+                const { centre: [h, k], radius: r } = parameters;
+                context.beginPath();
                 context.arc(h, k, r, 0, Math.PI * 2);
+                context.closePath();
                 break;
             case 'rectangle':
-                const [a,b,c,d] = parameters.vertices;
-                context.rect(a,b,c,d);
+                const { vertices: [x, y, width, height] } = parameters;
+                context.beginPath();
+                context.rect(x, y, width, height);
+                context.closePath();
                 break;
-            case 'triangle':
-                const [a,b,c] = parameters.vertices;
-                context.rect(a,b,c);
-                createStar()
-            case 'star':
-                context.rect(...parameters.vertices);
-                createStar()
             case 'aerofoil':
-                const aerofoilVertices = 
-                
-
-            // Add other shapes here
+                context.beginPath();
+                const vertices = createAerofoilVertices()
+                vertices.forEach(([x,y], i) => (i === 0) ? context.moveTo(x,y) : context.lineTo(x,y));
+                context.closePath();
+                break;
             default:
-                // Default case
+                context.beginPath();
+                parameters.vertices.forEach(([x,y], i) => (i === 0) ? context.moveTo(x,y) : context.lineTo(x,y));
+                context.closePath();
         }
 
         context.stroke(); // or context.fill() based on requirement
-    }, [type, size, centre, resolution]);
+    }, [shapeSettings]); // Use shapeSettings directly in the dependency array
+
 
     return (
         <canvas 
